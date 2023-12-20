5cd93656870f2760b943a4c706f625b5ce923163
Olena <petrova1512@gmail.com>
2023-12-20 19:47:05 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/stackNinjas-frontend">
-      {/* <Provider store={store}>
-        <App />
-      </Provider> */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
