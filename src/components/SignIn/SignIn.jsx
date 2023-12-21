568d705f3bb468b2379198da5f7d68d1448e10c1
Olena <petrova1512@gmail.com>
2023-12-21 21:46:59 +0100

    Update SignIn.jsx

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,7 +1,17 @@
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { useDispatch } from 'react-redux';
-import { BottleStyled, ErMsg, ForFormContainer, FormBtnStyled, SightInContainer, SightUp, StyledBtn, StyledField, StyledForm, Styledlabel } from './SignIn.styled';
+import {
+  BottleStyled,
+  ErMsg,
+  FormBtnStyled,
+  SightInContainer,
+  SightUp,
+  StyledBtn,
+  StyledField,
+  StyledForm,
+  Styledlabel,
+} from './SignIn.styled';
 import { logIn } from '../../redux/auth/operations';
 
 import iconeye from '../../images/show_icon.svg';
@@ -9,77 +19,86 @@ import hidepas from '../../images/hide_icon.svg';
 import { useState } from 'react';
 
 const SignupSchema = Yup.object().shape({
-    email: Yup.string()
+  email: Yup.string()
     .email('Invalid email address')
-    .matches(/^[-?\w.?%?]+@\w+.{1}\w{2,4}$/, 'Enter a valid email. For example user@gmail.com')
+    .matches(
+      /^[-?\w.?%?]+@\w+.{1}\w{2,4}$/,
+      'Enter a valid email. For example user@gmail.com'
+    )
     .required('Required'),
   password: Yup.string()
     .min(8, 'Too Short!')
     .max(48, 'Too Long!')
     .matches(/[a-zA-Z]/, 'Must contain at least one letter')
     .required('Required'),
-  });
-  
+});
 
-  export const SignIn  = ( ) =>{
-    const [showPassword, setShowPassword] = useState(false);
+export const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
 
-   
-    const dispatch = useDispatch();
-      return <>
-          <SightInContainer>
-          <Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        validationSchema={SignupSchema}
-        onSubmit={(values, action) => {
-          action.resetForm();
-          dispatch(logIn({
-            email: values.email,
-            password: values.password,
-          }));
-        }
-      }
-      > 
-        <StyledForm>
+  const dispatch = useDispatch();
+  return (
+    <>
+      <SightInContainer>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          validationSchema={SignupSchema}
+          onSubmit={(values, action) => {
+            action.resetForm();
+            dispatch(
+              logIn({
+                email: values.email,
+                password: values.password,
+              })
+            );
+          }}
+        >
+          <StyledForm>
             <h2>Sign In</h2>
-          <Styledlabel htmlFor="email">Enter your email
-          <StyledField id="email" name="email" 
-          placeholder="E-mail" 
-          />
-          <ErMsg component="span" name="email" />
-        </Styledlabel>
-          <Styledlabel htmlFor="password">Enter your password
-          <StyledField id="password" 
-              type={showPassword ? 'text' : 'password'}
-              name="password"   
-              placeholder="Password" 
-              title="password"
-           />
-           <StyledBtn onClick={() => setShowPassword(!showPassword)}>
-              {showPassword ? (
-                <img src={iconeye} width={18} height={18} alt="Hide Password" />
-              ) : (
-                <img
-                  src={hidepas}
-                  IconEye
-                  width={18}
-                  height={18}
-                  alt="Show Password"
-                />
-              )}
-            </StyledBtn>
+            <Styledlabel htmlFor="email">
+              Enter your email
+              <StyledField id="email" name="email" placeholder="E-mail" />
+              <ErMsg component="span" name="email" />
+            </Styledlabel>
+            <Styledlabel htmlFor="password">
+              Enter your password
+              <StyledField
+                id="password"
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                placeholder="Password"
+                title="password"
+              />
+              <StyledBtn onClick={() => setShowPassword(!showPassword)}>
+                {showPassword ? (
+                  <img
+                    src={iconeye}
+                    width={18}
+                    height={18}
+                    alt="Hide Password"
+                  />
+                ) : (
+                  <img
+                    src={hidepas}
+                    IconEye
+                    width={18}
+                    height={18}
+                    alt="Show Password"
+                  />
+                )}
+              </StyledBtn>
             </Styledlabel>
             <ErMsg component="span" name="password" />
-          <FormBtnStyled type="submit">Sign In</FormBtnStyled>
+            <FormBtnStyled type="submit">Sign In</FormBtnStyled>
 
-          <SightUp>Sight Up</SightUp>
-        </StyledForm>
-       
-      </Formik> 
-           <BottleStyled />
-        </SightInContainer>
-         </>
-  }
+            <SightUp>Sight Up</SightUp>
+          </StyledForm>
+        </Formik>
+        <BottleStyled />
+      </SightInContainer>
+    </>
+  );
+};
