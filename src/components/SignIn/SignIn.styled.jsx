af7e74d8c14ef5bd7e055601ef024db84f7ee7af
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-12-22 12:04:27 +0100

    Restricted rout

--- src/components/SignIn/SignIn.styled.jsx
+++ src/components/SignIn/SignIn.styled.jsx
@@ -97,9 +97,13 @@ export const StyledField = styled(Field)`
     flex-direction: column;
     align-items: flex-start;
     gap: 10px;
+    outline: none;
     border-radius: 6px;
     border: 1px solid ${theme.colors.secondary6};
     background: ${theme.colors.primeryWhite};
+    &:invalid{
+        border-color: ${theme.colors.secondary3};
+    }
     
 
 `
