5d22318fe6d81b63145cd76cc8fb7100e219ba13
YaremaNatalia <yaremka.n@gmail.com>
2023-12-23 12:08:10 +0200

    add header mainPage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,27 @@
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
+
+import logo from '../../images/header/logo.svg';
+import {
+  HeaderStyled,
+  HeaderWrap,
+  LogoStyled,
+  StyledNavLink,
+} from './Header.styled';
+import { AuthNav } from 'components/AuthNav/AuthNav';
+import { UserLogo } from 'components/UserLogo/UserLogo';
+
 export const Header = () => {
-  return <></>;
+  const authentificated = useSelector(selectIsLoggedIn);
+  return (
+    <HeaderStyled>
+      <HeaderWrap>
+        <StyledNavLink to="/">
+          <LogoStyled to="/" src={logo} alt="Logo" />
+        </StyledNavLink>
+        {authentificated ? <UserLogo /> : <AuthNav />}
+        {/* {authentificated ? <AuthNav /> : <UserLogo />} */}
+      </HeaderWrap>
+    </HeaderStyled>
+  );
 };
