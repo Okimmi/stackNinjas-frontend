92678f30b5f87b828866a5e55637b947f22785d9
Olena <petrova1512@gmail.com>
2023-12-23 22:20:48 +0100

    fix background main page

--- src/pages/MainPage/MainPage.styled.js
+++ src/pages/MainPage/MainPage.styled.js
@@ -5,13 +5,11 @@ import bgImgdesk from '../../images/mainPage/BgMainPage-desk.png';
 import bgImElemgdesk from '../../images/mainPage/BgElMainPage-desk.png';
 
 export const MainPageContainer = styled.div`
-  padding-top: 72px;
   display: flex;
   flex-direction: column;
   gap: 40px;
 
   @media screen and (min-width: 768px) {
-    padding-top: 88px;
     gap: 60px;
   }
 
@@ -19,31 +17,37 @@ export const MainPageContainer = styled.div`
     flex-direction: row;
     align-items: flex-end;
     gap: 81px;
-    padding: 128px 0 0 110px;
+    padding: 56px 0 0 110px;
   }
 `;
 
 export const BGImgMainPage = styled.div`
-  height: 100%;
   position: absolute;
-  top: 140px;
+  top: 109px;
   left: 0;
   right: 0;
   bottom: 0;
   z-index: -1;
 
-  background-image: url(${bgImgmob});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
+  @media screen and (max-width: 767px) {
+    height: 100%;
+    background-image: url(${bgImgmob});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+
   @media screen and (min-width: 768px) {
     background-image: url(${bgImgtab});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
   }
 
   @media screen and (min-width: 1440px) {
     background-image: url(${bgImElemgdesk}), url(${bgImgdesk});
-    background-size: cover, cover;
-    background-position: center, center;
+    background-size: contain;
+    background-position: top, bottom;
     background-repeat: no-repeat, no-repeat;
   }
 `;
