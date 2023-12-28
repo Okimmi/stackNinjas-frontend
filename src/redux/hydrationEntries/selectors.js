f94795096b3991f3477a43420e9d18bc65fe4b8d
SSSerg05 <ssserg05@gmail.com>
2023-12-28 20:40:59 +0200

    add selectors
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/redux/hydrationEntries/selectors.js
+++ src/redux/hydrationEntries/selectors.js
@@ -1,6 +1,7 @@
 // Contacts
 export const selectEntiesToday = state => state.hydrationEnties.items;
-export const selectEntiesMonth = state => state.hydrationEnties.items;
+export const selectEntiesMonth = state => state.hydrationEnties.itemsMonth;
+export const selectEntiesProgressToday = state => state.hydrationEnties.progressToday;
 //export const selectEntry = state => state.hydrationEnties.items;
 export const selectLoading = state => state.hydrationEnties.loading;
 export const selectError = state => state.hydrationEnties.error;
