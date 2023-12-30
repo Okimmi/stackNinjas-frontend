0cd093c26bcb08251b3575ac634db676d14e18e8
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2023-12-30 09:46:43 +0200

    fixed update user data

--- src/components/SettingModal/SettingUploadPhoto/SettingUploadPhoto.jsx
+++ src/components/SettingModal/SettingUploadPhoto/SettingUploadPhoto.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import logo from '../icons/Ellipse 1.jpg';
+import defaultAvatar from '../icons/default_user_avatar.svg';
 import {
   Title,
   MainWrapper,
@@ -8,7 +8,6 @@ import {
   UploadWrapper,
   Label,
   IconUploadImage,
-  UploadBtn,
   IconUser,
 } from '../SettingModal.styled';
 import { ToastContainer, toast } from 'react-toastify';
@@ -17,14 +16,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectAvatar } from '../../../redux/auth/selectors';
 import { refreshUser } from '../../../redux/auth/operations';
 
-const BASE_URL = 'https://stackninjas-backend.onrender.com';
+export const BASE_URL = 'https://stackninjas-backend.onrender.com';
 
 export const UploadPhoto = () => {
   const input = useRef();
   const dispatch = useDispatch();
   const current = useSelector(selectAvatar);
 
-  const [img, setImg] = useState();
+  const [img, setImg] = useState(null);
   const [avatar, setAvatar] = useState(current);
 
   const sendFile = async () => {
@@ -38,19 +37,26 @@ export const UploadPhoto = () => {
 
       formData.append('avatar', img);
 
-      await axios
-        .patch(`${BASE_URL}/api/auth/avatars`, formData, {
+      const { data } = await axios.patch(
+        `${BASE_URL}/api/auth/avatars`,
+        formData,
+        {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
-        })
-        .then(response => {
-          setAvatar(response.data.avatar);
-          dispatch(refreshUser());
-        });
+        }
+      );
+
+      setAvatar(data.avatar);
+      dispatch(refreshUser());
     } catch (error) {
       console.log(error.message);
     }
+  };
+
+  const handleFileChange = e => {
+    setImg(e.target.files[0]);
+    sendFile();
   };
 
   return (
@@ -62,22 +68,20 @@ export const UploadPhoto = () => {
         {avatar ? (
           <IconUser src={avatar} alt="user_photo" />
         ) : (
-          <IconUser src={logo} alt="user_photo" />
+          <IconUser src={defaultAvatar} alt="user_default_photo" />
         )}
         <UploadWrapper>
           <Label>
+            <IconUploadImage />
             <UploadInput
               ref={input}
               type="file"
               id="file-input"
-              onChange={e => {
-                setImg(e.target.files[0]);
-              }}
-              accept="image/*, .png,.jpeg,.gif, .web"
+              onChange={handleFileChange}
+              accept="image/*, .png, .jpeg, .gif, .web"
             />
-            <IconUploadImage />
+            Upload a photo
           </Label>
-          <UploadBtn onClick={sendFile}>Upload a photo</UploadBtn>
         </UploadWrapper>
         <ToastContainer />
       </MainWrapper>
