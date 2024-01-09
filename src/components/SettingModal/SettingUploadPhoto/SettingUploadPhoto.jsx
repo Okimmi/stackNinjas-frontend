e8a4258342b4d5df2e15d2dab497f2494afd5524
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2024-01-09 14:11:23 +0200

    in progress

--- src/components/SettingModal/SettingUploadPhoto/SettingUploadPhoto.jsx
+++ src/components/SettingModal/SettingUploadPhoto/SettingUploadPhoto.jsx
@@ -7,41 +7,69 @@ import {
   Label,
   IconUploadImage,
   IconUser,
-  UploaLoader,
-  UploadLoaderWrapper,
+  ProgressBar,
+  ProgressBarContainer,
+  // UploaLoader,
+  // UploadLoaderWrapper,
 } from '../SettingModal.styled';
 import { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectAvatar } from '../../../redux/auth/selectors';
-import { updateAvatar } from '../../../redux/auth/operations';
+import {
+  // setUploadProgress,
+  updateAvatar,
+} from '../../../redux/auth/operations';
 
 export const UploadPhoto = () => {
   const input = useRef();
   const dispatch = useDispatch();
   const currentAvatar = useSelector(selectAvatar);
 
-  const [loadingAvatar, setLoadingAvatar] = useState(false);
+  // const [loadingAvatar, setLoadingAvatar] = useState(false);
+
+  const [progress, setProgress] = useState(0);
 
   const handleFileChange = async e => {
     try {
-      setLoadingAvatar(true);
+      setProgress(0);
       await dispatch(updateAvatar(e.target.files[0]));
     } catch (error) {
-      console.error(error.message);
-    } finally {
-      setLoadingAvatar(false);
+      console.log(error.message);
     }
+    // try {
+    //   setLoadingAvatar(true);
+    //   await dispatch(updateAvatar(e.target.files[0]));
+    // } catch (error) {
+    //   console.error(error.message);
+    // } finally {
+    //   setLoadingAvatar(false);
+    // }
   };
 
   return (
     <>
       <Title>Your photo</Title>
       <MainWrapper>
-        {loadingAvatar ? (
+        {/* {loadingAvatar ? (
           <UploadLoaderWrapper>
             <UploaLoader />
           </UploadLoaderWrapper>
         ) : (
+          <IconUser src={currentAvatar || defaultAvatar} alt="user_photo" />
+        )} */}
+
+        {/* {progress && ( */}
+        <ProgressBarContainer>
+          <ProgressBar
+            style={{ width: progress + '%' }}
+            value={progress}
+            max="100"
+          />
+          <p>{progress}%</p>
+        </ProgressBarContainer>
+        {/* )} */}
+
+        {!progress && (
           <IconUser src={currentAvatar || defaultAvatar} alt="user_photo" />
         )}
 
