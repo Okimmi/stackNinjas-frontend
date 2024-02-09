2d34ae1749969cd0400d7b9f003bc4547e2d6e19
Olena <petrova1512@gmail.com>
2024-02-09 20:36:32 +0100

    Update SharedLayout.jsx

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -1,13 +1,18 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './Header/Header';
 import { Wrapper } from './Global/Global.styled';
 import { Footer } from './Footer/Footer';
 import Loader from './Loader/Loader';
 import { useAuth } from '../redux/hooks/useAuth';
+import { $instance } from '../redux/constants';
 
 export const SharedLayout = () => {
   const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    $instance.get('/health');
+  }, []);
 
   return (
     <>
