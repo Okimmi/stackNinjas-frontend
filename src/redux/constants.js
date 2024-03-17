2e86acb766e8b0b5bdf94d75bd44d3355b9e8166
Olena <petrova1512@gmail.com>
2024-03-17 17:38:53 +0100

    Update constants.js

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
 export const $instance = axios.create({
-  baseURL: 'https://stackninjas-backend.onrender.com',
+  baseURL: 'https://stack-ninjas-backend.vercel.app',
 });
-
-
